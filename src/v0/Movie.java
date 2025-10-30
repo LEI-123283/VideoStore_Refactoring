a1f14fcb48c61e1454a0dd682b628f5b8e314edd
Lei-123283 <gcnma@iscte-iul.pt>
2025-10-30 18:27:05 +0000

    Move and extract methods in Customer.java

--- src/v0/Movie.java
+++ src/v0/Movie.java
@@ -2,11 +2,11 @@ package v0;
 
 public class Movie {
 
-	public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
-	
+	public enum Code { REGULAR, CHILDRENS, NEW_RELEASE }
+
 	private String _title;
 	private Code _priceCode;
-	
+
 	public Movie(String title, Code priceCode) {
 		_title = title;
 		_priceCode = priceCode;
@@ -18,5 +18,25 @@ public class Movie {
 
 	public Code getPriceCode() {
 		return _priceCode;
+	}
+
+	public double calculateAmount(int daysRented) {
+		double amount = 0;
+		switch (_priceCode) {
+			case REGULAR:
+				amount += 2;
+				if (daysRented > 2)
+					amount += (daysRented - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				amount += daysRented * 3;
+				break;
+			case CHILDRENS:
+				amount += 1.5;
+				if (daysRented > 3)
+					amount += (daysRented - 3) * 1.5;
+				break;
+		}
+		return amount;
 	}
 }
--- src/v0/Rental.java
+++ src/v0/Rental.java
@@ -1,23 +1,31 @@
 package v0;
 
-public class Rental
-{
-	private Movie	_movie;
-	private int		_daysRented;
+public class Rental {
+	private Movie _movie;
+	private int _daysRented;
 
-	public Rental(Movie movie, int daysRented)
-	{
+	public Rental(Movie movie, int daysRented) {
 		_movie = movie;
 		_daysRented = daysRented;
 	}
 
-	public int getDaysRented()
-	{
+	public int getDaysRented() {
 		return _daysRented;
 	}
 
-	public Movie getMovie()
-	{
+	public Movie getMovie() {
 		return _movie;
+	}
+
+	// métodos de instância
+	public double getThisAmount() {
+		return _movie.calculateAmount(_daysRented);
+	}
+
+	public int getFrequentRenterPoints() {
+		int points = 1;
+		if (_movie.getPriceCode() == Movie.Code.NEW_RELEASE && _daysRented > 1)
+			points++;
+		return points;
 	}
 }
