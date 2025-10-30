a9a21f6d3858c3b4f5b2f027a04c8204c9752580
LEI-123293 <icpls@iscte-iul.pt>
2025-10-30 23:12:41 +0000

    Replace Temp with Query: getTotalAmount and getTotalFrequentRenterPoints(v5)

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -24,28 +24,36 @@ public class Customer
 
 	public String statement()
 	{
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-
 		// header
 		String result = "Rental Record for " + getName() + "\n";
 
-		for (Rental each: _rentals)
+		for (Rental each : _rentals)
 		{
-			// determine amounts for each line
-			double thisAmount = each.getAmount();
-
-			// add frequent renter points
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
 		}
 
 		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
+		result += "Amount owed is " + getTotalAmount() + "\n";
+		result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
 		return result;
 	}
+
+
+	private double getTotalAmount() {
+		double totalAmount = 0;
+		for (Rental each : _rentals) {
+			totalAmount += each.getAmount();
+		}
+		return totalAmount;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int frequentRenterPoints = 0;
+		for (Rental each : _rentals) {
+			frequentRenterPoints += each.getFrequentRenterPoints();
+		}
+		return frequentRenterPoints;
+	}
+
 }
